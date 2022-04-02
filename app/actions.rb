14973040485f35105a0357bcdbf55eed9822ab3b
ghoulishcat <thisisthrillerr@gmail.com>
2022-04-02 22:33:02 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,13 @@
+helpers do
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+  end
+
+
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
-
+    
     erb(:index)
 end
 
@@ -11,18 +18,41 @@ get '/signup' do     # if a user navigates to the path "/signup",
 
   post '/signup' do
     
-    email       = params[:email]
-    avatar_url  = params[:avatar_url]
-    username    = params[:username]
-    password    = params[:password]
-
+    email      = params[:email]
+    avatar_url = params[:avatar_url]
+    username   = params[:username]
+    password   = params[:password]
+  
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
-    
+  
     if @user.save
-        "User #{username} saved!"
+      redirect to('/login')
     else
-        erb(:signup)
+      erb(:signup)
         
     end
+end
 
+    get '/login' do    # when a GET request comes into /login
+        erb(:login)      # render app/views/login.erb
+      end
+
+post '/login' do    
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
   end
+end
+
+get '/logout' do
+    session[:user_id] = nil 
+    redirect to('/')
+end
